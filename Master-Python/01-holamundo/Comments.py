12a52a232aa6a3c1839180f39c9b62b5073d51cc
Victor Muntane Fuentes <vmuntane96it@gmail.com>
2025-07-23 17:34:53 +0200

    Feat: add Comments.py

---
+++ Master-Python/01-holamundo/Comments.py
@@ -0,0 +1,10 @@
+# Esto me mostrara el texto hola por la consola
+print("hola")
+# Estas dos instrucciones me mostraran Mundo y !!
+print("Mundo")
+print("!!")
+"""
+esto no se mostrará
+print("Esto no se va a mostrar por la consola")
+print("porque estará dentro de un comentario multilineal")
+"""
