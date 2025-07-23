71dd8d817255bb6a310870397aa598f5802b3673
Victor Muntane Fuentes <vmuntane96it@gmail.com>
2025-07-23 19:37:01 +0200

    feat: add arithmetic.py

---
+++ Master-Python/03-operators/arithmetic.py
@@ -0,0 +1,19 @@
+#Operadores aritméticos
+#Operadores de asignación =
+numero1 = 77
+numero2 = 44
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+resto = numero1 % numero2
+potencia = numero1 ** 2
+
+print("*******************CALCULADORA*******************")
+print(f"La suma es: {numero1 + numero2}")
+print(f"La resta es: {resta}")
+print(f"La multiplicacion es: {multiplicacion}")
+print(f"La division es: {division}")
+print(f"El resto de la division es: {resto}")
+print(f"La potencia es: {potencia}")
+
