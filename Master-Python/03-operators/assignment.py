28154cb71c729a7745654b8d8c68102c171a6975
Victor Muntane Fuentes <vmuntane96it@gmail.com>
2025-07-23 19:51:02 +0200

    feat: add assignment.py

---
+++ Master-Python/03-operators/assignment.py
@@ -0,0 +1,30 @@
+#Operadores de asignación
+edad = 55
+
+print(edad)
+
+# Incrementa la edad en 5
+edad += 5
+print(edad)  
+edad -= 10
+print(edad)
+
+#Operadores de incremento y decremento
+year = 2025
+
+# Incrementa el año en 1
+year = year + 1
+print(year) 
+
+# Decrementa el año en 1
+year -= 1
+print(year) 
+
+#Operadores de preincremento y predecremento
+# Preincremento
+year = 1 + year
+print(year)  
+
+# Predecremento
+year= 1 - year
+print(year)
